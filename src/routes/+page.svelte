0517a9a7a72dfc0f164241897a5cba2688998ce1
Archie Wood <58074498+archiewood@users.noreply.github.com>
2022-11-09 18:24:19 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Prism from 'svelte-prism'
+</script>
+
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism-okaidia.min.css'>
+
+<Prism language="html">
+    {`
+      <header>
+        <h1>Hello World!</h1>
+      </header>
+    `}
+</Prism>
+
